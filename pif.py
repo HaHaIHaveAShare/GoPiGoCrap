52d1e6a8af874f34f9553df7cd1bd696d56e1fb9
HaHaIHaveAShare <43505647+HaHaIHaveAShare@users.noreply.github.com>
2018-11-15 19:57:53 -0800

    added user defined g9oals

--- pif.py
+++ pif.py
@@ -1,32 +1,53 @@
 import gopigo3
 import time
+from di_sensors.easy_distance_sensor import EasyDistanceSensor
 import easygopigo3 as easy
 
 mrBean = gopigo3.GoPiGo3()
 easyBean = easy.EasyGoPiGo3()
 
-c = 0
-
-error = 0
-integral = 0
-feedForward = 1
-
 maximum = 255
 
-point = 4000.0
+p = .4
+k = .1
 
-p = .2
-k = .2
+mrDisty = EasyDistanceSensor()
 
-mrBean.offset_motor_encoder(mrBean.MOTOR_LEFT, mrBean.get_motor_encoder(mrBean.MOTOR_LEFT))
+corrective = 1.7888571428571 # this constant corrects for error in the physical robot
 
-while mrBean.get_motor_encoder(mrBean.MOTOR_LEFT) <= point:
-    error = (point - mrBean.get_motor_encoder(mrBean.MOTOR_LEFT)) * p
-    integral += error
-    c = (error * p) + (integral * k) + feedForward
-    if c > maximum:
-        c = maximum
-    easyBean.set_speed(c)
-    easyBean.forward()
-    print("Distance: %d" % (mrBean.get_motor_encoder(mrBean.MOTOR_LEFT)))
-easyBean.stop()
+while True:
+
+    mrBean.offset_motor_encoder(mrBean.MOTOR_LEFT, mrBean.get_motor_encoder(mrBean.MOTOR_LEFT))
+
+    c = 0
+    
+    error = 0
+    integral = 0
+    feedForward = 1
+    try:
+        point = (float(input("Enter a distance in millimeters: ")))
+    except:
+        point = 0;
+    goBackward = False
+    if point <= 0:
+        goBackward = True
+        point *= -1
+        
+    point *= corrective
+    
+    readDisty = mrDisty.read()
+
+    while (goBackward == False and mrBean.get_motor_encoder(mrBean.MOTOR_LEFT) <= point or goBackward and mrBean.get_motor_encoder(mrBean.MOTOR_LEFT) >= point * -1) and (readDisty >= 10 or goBackward):
+        readDisty = mrDisty.read()
+        error = (point - mrBean.get_motor_encoder(mrBean.MOTOR_LEFT))
+        integral += error
+        c = (error * p) + (integral * k) + feedForward
+        if c > maximum:
+            c = maximum
+        easyBean.set_speed(c)
+        if goBackward == False:
+            easyBean.forward()
+        else:
+            easyBean.backward()
+        print("Distance: %d Goal: %d Wall Distance: %d" % (mrBean.get_motor_encoder(mrBean.MOTOR_LEFT) / corrective, point / corrective, readDisty))
+    easyBean.stop()
